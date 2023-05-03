b2d3f3af009d6434e4d6261ad84b3a6a72745552
hnyls2002 <hnyls2002@gmail.com>
2023-05-03 20:28:13 +0800

    🐞 fix(heap_alloc): add alloc_error_handler

--- os/src/mm/heap_allocator/mod.rs
+++ os/src/mm/heap_allocator/mod.rs
@@ -14,6 +14,11 @@ static HEAP_ALLOCATOR: UPSafeHeap = UPSafeHeap::new();
 // heap space for kernel
 static mut HEAP_SPACE: [u8; KERNEL_HEAP_SIZE] = [0; KERNEL_HEAP_SIZE];
 
+#[alloc_error_handler]
+pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
+    panic!("Heap allocation error, layout = {:#X?}", layout);
+}
+
 pub fn heap_init() {
     unsafe {
         HEAP_ALLOCATOR
