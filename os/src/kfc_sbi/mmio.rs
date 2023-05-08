fd23fb945f3e6f9e8d47daa71589d7515f2051f4
hnyls2002 <hnyls2002@gmail.com>
2023-05-08 10:38:30 +0800

    🐞 fix(mmio): for clint : get_time & get_timecmp

--- os/src/kfc_sbi/mmio.rs
+++ os/src/kfc_sbi/mmio.rs
@@ -10,4 +10,9 @@ const UART_MMIO: VPRange = VPRange {
     end: Page(0x1000_0000 + 0x1000),
 };
 
-pub const MMIO: [VPRange; 2] = [VIRT_MMIO, UART_MMIO];
+const CLINT_MMIO: VPRange = VPRange {
+    start: Page(0x200_0000),
+    end: Page(0x200_0000 + 0x10000),
+};
+
+pub const MMIO: [VPRange; 3] = [VIRT_MMIO, UART_MMIO, CLINT_MMIO];
